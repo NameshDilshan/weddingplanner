d48d0b5bdfb5e6eb03962135560ab5c13b7b5820
NameshDilshan <nameshdilshan@gmail.com>
2023-04-01 21:30:47 +0530

    login function created

--- lib/pages/login_register_page.dart
+++ lib/pages/login_register_page.dart
@@ -1,8 +1,11 @@
+import 'dart:core';
+import 'dart:core';
+import 'dart:core';
+import 'dart:core';
+
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
-import 'package:weddingplanner/models/User.dart';
-import '../auth.dart';
 
 class LoginPage extends StatefulWidget {
   const LoginPage({Key? key}) : super(key: key);
@@ -14,20 +17,55 @@ class LoginPage extends StatefulWidget {
 class _LoginPageState extends State<LoginPage> {
   String? errorMessage = '';
   bool isLogin = true;
-  final GlobalKey _formKey = GlobalKey<FormState>();
-  final List<String> textFieldsValue = [];
+
+  // final GlobalKey<ScaffoldState> _scaffoldKey = GlobalKey(); //so we can call snackbar from anywhere
+  final _formKey = GlobalKey<FormState>();
+
+  // final List<String> textFieldsValue = []
+  final Map<String, TextEditingController> registerController = {
+    'username': TextEditingController(),
+    'firstname': TextEditingController(),
+    'lastname': TextEditingController(),
+    'age': TextEditingController(),
+    'password': TextEditingController(),
+    'location': TextEditingController(),
+    'phoneno': TextEditingController(),
+    'email': TextEditingController(),
+  };
 
   // final TextEditingController _controllerEmail = TextEditingController();
   // final TextEditingController _controllerPassword = TextEditingController();
 
-  Future<void> signInWithformdata() async {
+  Future<String?> signInWithformdata() async {
+    print(registerController['username']?.text);
     try {
-      // await Auth().signInWithEmailAndPassword(email: _controllerEmail.text, password: _controllerPassword.text);
-    } on FirebaseAuthException catch (e) {
-      setState(() {
-        errorMessage = e.message;
+      final docuser = FirebaseFirestore.instance.collection('users').doc();
+      await docuser.set({
+        'username': registerController['username']?.text,
+        'firstname': registerController['firstname']?.text,
+        'lastname': registerController['lastname']?.text,
+        'age': registerController['age']?.text,
+        'password': registerController['password']?.text,
+        'location': registerController['location']?.text,
+        'phoneno': registerController['phoneno']?.text,
+        'email': registerController['email']?.text,
       });
+      return 'success';
+    } catch (e) {
+      setState(() {
+        errorMessage = e.toString();
+      });
+      return e.toString();
     }
+
+    // try {
+    //   controller: registerController['email'],
+    //   // await Auth().signInWithEmailAndPassword(email: _controllerEmail.text, password: _controllerPassword.text);
+    // } on FirebaseAuthException catch (e) {
+    //   setState(() {
+    //     errorMessage = e.message;
+    //   });
+    // }
   }
 
   Widget loginWidget() {
@@ -36,6 +74,7 @@ class _LoginPageState extends State<LoginPage> {
         SizedBox(
           width: MediaQuery.of(context).size.width * 70 / 100,
           child: TextFormField(
+            controller: registerController['email'],
             decoration: const InputDecoration(
               hintText: 'Email',
             ),
@@ -50,6 +89,7 @@ class _LoginPageState extends State<LoginPage> {
         SizedBox(
           width: MediaQuery.of(context).size.width * 70 / 100,
           child: TextFormField(
+            controller: registerController['password'],
             decoration: const InputDecoration(
               hintText: 'Password',
             ),
@@ -66,121 +106,172 @@ class _LoginPageState extends State<LoginPage> {
   }
 
   Widget registerWidget() {
-    return Column(
-      children: [
-        SizedBox(
-          width: MediaQuery.of(context).size.width * 70 / 100,
-          child: TextFormField(
-            decoration: const InputDecoration(
-              hintText: 'Email',
+    List<String> _locations = ['A', 'B', 'C', 'D']; // Option 2
+    String? _selectedLocation = ""; // Option 2
+    String? dropDownValue;
+
+    return Form(
+      key: _formKey,
+      child: Column(
+        children: [
+          SizedBox(
+            width: MediaQuery.of(context).size.width * 70 / 100,
+            child: TextFormField(
+              controller: registerController['email'],
+              decoration: const InputDecoration(
+                hintText: 'Email',
+              ),
+              validator: (value) {
+                if (value == null || value.isEmpty) {
+                  return 'Please enter some Email';
+                }
+                return null;
+              },
             ),
-            validator: (value) {
-              if (value == null || value.isEmpty) {
-                return 'Please enter some Email';
-              }
-              textFieldsValue.add(value!);
-            },
           ),
-        ),
-        // SizedBox(
-        //   width: MediaQuery.of(context).size.width * 70 / 100,
-        //   child: TextFormField(
-        //     decoration: const InputDecoration(
-        //       hintText: 'Password',
-        //     ),
-        //     validator: (value) {
-        //       if (value == null || value.isEmpty) {
-        //         return 'Please enter Password';
-        //       }
-        //     },
-        //   ),
-        // ),
-        // SizedBox(
-        //   width: MediaQuery.of(context).size.width * 70 / 100,
-        //   child: TextFormField(
-        //     decoration: const InputDecoration(
-        //       hintText: 'Username',
-        //     ),
-        //     validator: (value) {
-        //       if (value == null || value.isEmpty) {
-        //         return 'Please enter Username';
-        //       }
-        //       return null;
-        //     },
-        //   ),
-        // ),
-        // SizedBox(
-        //   width: MediaQuery.of(context).size.width * 70 / 100,
-        //   child: TextFormField(
-        //     decoration: const InputDecoration(
-        //       hintText: 'First Name',
-        //     ),
-        //     validator: (value) {
-        //       if (value == null || value.isEmpty) {
-        //         return 'Please enter First Name';
-        //       }
-        //       return null;
-        //     },
-        //   ),
-        // ),
-        // SizedBox(
-        //   width: MediaQuery.of(context).size.width * 70 / 100,
-        //   child: TextFormField(
-        //     decoration: const InputDecoration(
-        //       hintText: 'Last Name',
-        //     ),
-        //     validator: (value) {
-        //       if (value == null || value.isEmpty) {
-        //         return 'Please enter Last Name';
-        //       }
-        //       return null;
-        //     },
-        //   ),
-        // ),
-        // SizedBox(
-        //   width: MediaQuery.of(context).size.width * 70 / 100,
-        //   child: TextFormField(
-        //     keyboardType: TextInputType.number,
-        //     decoration: const InputDecoration(
-        //       hintText: 'Age',
-        //     ),
-        //     validator: (value) {
-        //       if (value == null || value.isEmpty) {
-        //         return 'Please enter Age';
-        //       }
-        //       return null;
-        //     },
-        //   ),
-        // ),
-        // SizedBox(
-        //   width: MediaQuery.of(context).size.width * 70 / 100,
-        //   child: TextFormField(
-        //     decoration: const InputDecoration(
-        //       hintText: 'Location',
-        //     ),
-        //     validator: (value) {
-        //       if (value == null || value.isEmpty) {
-        //         return 'Please enter Location';
-        //       }
-        //       return null;
-        //     },
-        //   ),
-        // ),
-        // SizedBox(
-        //   width: MediaQuery.of(context).size.width * 70 / 100,
-        //   child: TextFormField(
-        //     decoration: const InputDecoration(
-        //       hintText: 'Phone No',
-        //     ),
-        //     validator: (value) {
-        //       if (value == null || value.isEmpty) {
-        //         return 'Please enter Phone No';
-        //       }
-        //       return null;
-        //     },
-        //   ),
-        // ),
-      ],
+          SizedBox(
+            width: MediaQuery.of(context).size.width * 70 / 100,
+            child: TextFormField(
+              controller: registerController['password'],
+              decoration: const InputDecoration(
+                hintText: 'Password',
+              ),
+              validator: (value) {
+                if (value == null || value.isEmpty) {
+                  return 'Please enter Password';
+                }
+                return null;
+              },
+            ),
+          ),
+          SizedBox(
+            width: MediaQuery.of(context).size.width * 70 / 100,
+            child: TextFormField(
+              controller: registerController['username'],
+              decoration: const InputDecoration(
+                hintText: 'Username',
+              ),
+              validator: (value) {
+                if (value == null || value.isEmpty) {
+                  return 'Please enter Username';
+                }
+                return null;
+              },
+            ),
+          ),
+          SizedBox(
+            width: MediaQuery.of(context).size.width * 70 / 100,
+            child: TextFormField(
+              controller: registerController['firstname'],
+              decoration: const InputDecoration(
+                hintText: 'First Name',
+              ),
+              validator: (value) {
+                if (value == null || value.isEmpty) {
+                  return 'Please enter First Name';
+                }
+                return null;
+              },
+            ),
+          ),
+          SizedBox(
+            width: MediaQuery.of(context).size.width * 70 / 100,
+            child: TextFormField(
+              controller: registerController['lastname'],
+              decoration: const InputDecoration(
+                hintText: 'Last Name',
+              ),
+              validator: (value) {
+                if (value == null || value.isEmpty) {
+                  return 'Please enter Last Name';
+                }
+                return null;
+              },
+            ),
+          ),
+          SizedBox(
+            width: MediaQuery.of(context).size.width * 70 / 100,
+            child: TextFormField(
+              controller: registerController['age'],
+              keyboardType: TextInputType.number,
+              decoration: const InputDecoration(
+                hintText: 'Age',
+              ),
+              validator: (value) {
+                if (value == null || value.isEmpty) {
+                  return 'Please enter Age';
+                }
+                return null;
+              },
+            ),
+          ),
+          SizedBox(
+            width: MediaQuery.of(context).size.width * 70 / 100,
+            child: TextFormField(
+              controller: registerController['gender'],
+              decoration: const InputDecoration(
+                hintText: 'Gender',
+              ),
+              validator: (value) {
+                if (value == null || value.isEmpty) {
+                  return 'Please enter Gender';
+                }
+                return null;
+              },
+            ),
+          ),
+          SizedBox(
+              width: MediaQuery.of(context).size.width * 70 / 100,
+              child: DropdownButton(
+                hint: dropDownValue == null ? const Text('Dropdown') : Text(dropDownValue, style: const TextStyle(color: Colors.blue),
+                ),
+                isExpanded: true,
+                iconSize: 30.0,
+                style: const TextStyle(color: Colors.blue),
+                items: ['One', 'Two', 'Three'].map((val) {
+                    return DropdownMenuItem<String>(
+                      value: val,
+                      child: Text(val),
+                    );
+                  },
+                ).toList(),
+                onChanged: (val) {
+                  setState(() {
+                      dropDownValue = val;
+                    },
+                  );
+                },
+              ),
+              // TextFormField(
+              //   controller: registerController['location'],
+              //   decoration: const InputDecoration(
+              //     hintText: 'Location',
+              //   ),
+              //   validator: (value) {
+              //     if (value == null || value.isEmpty) {
+              //       return 'Please enter Location';
+              //     }
+              //     return null;
+              //   },
+              // ),
+              ),
+          SizedBox(
+            width: MediaQuery.of(context).size.width * 70 / 100,
+            child: TextFormField(
+              controller: registerController['phoneno'],
+              decoration: const InputDecoration(
+                hintText: 'Phone No',
+              ),
+              validator: (value) {
+                if (value == null || value.isEmpty) {
+                  return 'Please enter Phone No';
+                }
+                return null;
+              },
+            ),
+          ),
+        ],
+      ),
     );
   }
 
@@ -191,7 +282,11 @@ class _LoginPageState extends State<LoginPage> {
   Widget _submitButton() {
     return ElevatedButton(
       onPressed: () {
-          isLogin ? signInWithformdata() : createUserWithformdata();
+        if (_formKey.currentState != null) {
+          if (_formKey.currentState!.validate()) {
+            isLogin ? signInWithformdata() : createUserWithformdata();
+          }
+        }
       },
       child: Text(isLogin ? 'Login' : 'Register'),
     );
@@ -208,24 +303,35 @@ class _LoginPageState extends State<LoginPage> {
     );
   }
 
-  Future<void> createUserWithformdata() async {
-    // try {
-      print(textFieldsValue.length);
-      final docuser = FirebaseFirestore.instance.collection('users').doc();
-      // final user = User(
-      // id: docuser.id,
-      // username: username, firstname: firstname, lastname: lastname, age: age, password: password, location: location, phoneno: phoneno)
-      // await Auth().createUserWithEmailAndPassword(email: _controllerEmail.text, password: _controllerPassword.text);
-    // } on FirebaseAuthException catch (e) {
-    //   setState(() {
-    //     errorMessage = e.message;
-    //   });
-    // }
+  Future<String> createUserWithformdata() async {
+    try {
+      final docuser = FirebaseFirestore.instance
+          .collection('users')
+          .doc(registerController['email']?.text);
+      await docuser.set({
+        'email': registerController['email']?.text,
+        'password': registerController['password']?.text,
+        'username': registerController['username']?.text,
+        'firstname': registerController['firstname']?.text,
+        'lastname': registerController['lastname']?.text,
+        'age': registerController['age']?.text,
+        'location': registerController['location']?.text,
+        'phoneno': registerController['phoneno']?.text,
+        'gender': registerController['gender']?.text,
+      });
+      return 'success';
+    } catch (e) {
+      setState(() {
+        errorMessage = e.toString();
+      });
+      return e.toString();
+    }
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      // key: _scaffoldKey,
       body: Container(
         height: double.infinity,
         width: double.infinity,
@@ -245,7 +351,7 @@ class _LoginPageState extends State<LoginPage> {
                 const SizedBox(height: 20),
                 _errorMessage(),
                 const SizedBox(height: 50),
-              _submitButton(),
+                _submitButton(),
                 _loginOrRegisterButton()
               ],
             )),
